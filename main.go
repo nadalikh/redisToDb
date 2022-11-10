1ab2e761699434682993722d3089eeca767e535c
Nadali Khalili <nadalikh@gmai.com>
2022-11-10 09:13:10 +0330

    get keys

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"go.mongodb.org/mongo-driver/mongo"
+	. "fmt"
+	"github.com/go-redis/redis"
+	"log"
 )
 
 func main() {
-	mongo.NewClient()
-
+	var r = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err := r.Ping().Result()
+	if err != nil {
+		log.Fatal(err)
+	}
+	Println(r.Keys("[0-9]*"))
 }
