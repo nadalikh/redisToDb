dd64ceafbf19f0c0fcd93daeae151c14777158b6
Nadali Khalili <nadalikh@gmai.com>
2022-11-12 13:43:41 +0330

    complete without testing

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/go-redis/redis"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
 func main() {
+	//Redis connection
 	var r = redis.NewClient(&redis.Options{
 		Addr:     "127.0.0.1:6379",
 		Password: "",
@@ -15,13 +21,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, val := range r.LRange("111", 0, -1).Val() {
-		println(val)
+	//Creating mongodb connection
+	db, err := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	//keys := r.Keys("[0-9]*").Val()
-	//for _, key := range keys {
-	//	println(r.LRange("111", 0, -1).Val())
-	//	//r.RPop(key)
-	//}
+	//Start the connection
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var imeiDetail []interface{}
+	for {
+		keys := r.Keys("[0-9]*").Val()
+		for _, key := range keys {
+			numberOfDetailsForAnImei := len(r.LRange(key, 0, -1).Val())
+			for i := 0; i < numberOfDetailsForAnImei; i++ {
+				imeiDetail = append(imeiDetail, bson.D{{key, r.RPop(key)}})
+			}
+		}
+		collection := db.Database("GPS").Collection("statuses")
+		res, err := collection.InsertMany(ctx, imeiDetail)
+		if err != nil {
+			return
+		}
+		print(res.InsertedIDs)
+		imeiDetail = nil
+		time.Sleep(time.Second * 20)
+	}
 }
